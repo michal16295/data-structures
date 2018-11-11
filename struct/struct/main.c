ec87af6f4f3b9abb351107abc1d0a2a47bf6f087
FunnyGames <fgmmaker@gmail.com>
2018-11-11 20:48:00 +0200

    Dima changes

---
+++ struct/struct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  struct
+//
+//  Created by Dima Zagorodny on 11/11/2018.
+//  Copyright © 2018 Dima Zagorodny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
